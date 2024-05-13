07aecef8c684e10b92f6030dbb9708308476648f
faizhasans <165466580+faizhasans@users.noreply.github.com>
2024-05-13 13:48:56 +0700

    Update web.php
    use route /login

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('landing-page');
 });
 
-Route::get('/loginPage', function () {
+Route::get('/login', function () {
     return view('loginPage');
 });
 
